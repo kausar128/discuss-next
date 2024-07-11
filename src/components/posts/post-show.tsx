916b34939574c67fbd05c39fdf6979c8ab6f0c39
kausar128 <kausar128@gmail.com>
2024-07-11 11:32:05 +0530

    Added component to show a post

--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -1,10 +1,19 @@
-import type { Post } from "@prisma/client";
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
 
 interface PostShowProps {
-  post: Post;
+  postId: string;
 }
 
-export default function PostShow({ post }: PostShowProps) {
+export default async function PostShow({ postId }: PostShowProps) {
+  const post = await db.post.findFirst({
+    where: { id: postId },
+  });
+
+  if (!post) {
+    notFound();
+  }
+
   return (
     <div className="m-4">
       <h1 className="text-2xl font-bold my-2">{post.title}</h1>
