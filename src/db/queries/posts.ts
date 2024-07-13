c2ae28bd73f8c259908c53d04a81a80eca6b5377
kausar128 <kausar128@gmail.com>
2024-07-13 21:45:04 +0530

    request caching and Suspense for wrapping components

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,5 +1,5 @@
-import type { Post } from '@prisma/client';
-import { db } from '@/db';
+import type { Post } from "@prisma/client";
+import { db } from "@/db";
 
 export type PostWithData = Post & {
   topic: { slug: string };
