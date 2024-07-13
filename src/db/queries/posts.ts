aadc19e7bd4d617ba4d566332919b3fd4bc93a50
kausar128 <kausar128@gmail.com>
2024-07-13 22:04:08 +0530

    fetching top posts on home page by comments

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,5 +1,5 @@
-import type { Post } from "@prisma/client";
-import { db } from "@/db";
+import type { Post } from '@prisma/client';
+import { db } from '@/db';
 
 export type PostWithData = Post & {
   topic: { slug: string };
@@ -15,5 +15,23 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
       user: { select: { name: true } },
       _count: { select: { comments: true } },
     },
+  });
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: 'desc',
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
   });
 }
